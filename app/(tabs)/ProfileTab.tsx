b6587de63aae715af11c9066b9584353ecad918c
Sumanbassetti <128141423+Sumanbassetti@users.noreply.github.com>
2025-09-23 16:53:16 +0530

    feat: profile screen

--- app/(tabs)/ProfileTab.tsx
+++ app/(tabs)/ProfileTab.tsx
@@ -1,14 +1,64 @@
+import { Image } from "expo-image";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StatusBar, Text, View } from "react-native";
+import {
+  ArrowRightOnRectangleIcon,
+  ClipboardDocumentListIcon,
+  ShoppingCartIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "../../constants/theme";
+import MenuItem from "../components/MenuItem/MenuItem";
+import styles from "../tabStyles/ProfileTabStyle";
 
 const ProfileTab = () => {
+  const handleMenuPress = (item: string) => {
+    console.log(`${item} pressed`);
+    // Add navigation logic here
+  };
+
   return (
-    <View>
-      <Text>ProfileTab</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor={Colors.bg} />
+
+      <ScrollView style={styles.content} showsVerticalScrollIndicator={false}>
+        {/* Profile Section */}
+        <View style={styles.profileSection}>
+          <View style={styles.profileImageContainer}>
+            <Image
+              source={require("../../assets/icons/profile.png")}
+              style={styles.profileImage}
+              contentFit="contain"
+            />
+          </View>
+          <View style={styles.profileNameContainer}>
+            <Text style={styles.profileName}>Johnny</Text>
+          </View>
+        </View>
+        <MenuItem
+          icon={<UserIcon size={24} color={Colors.text} />}
+          title="Profile"
+          onPress={() => handleMenuPress("Profile")}
+        />
+        <MenuItem
+          icon={<ClipboardDocumentListIcon size={24} color={Colors.text} />}
+          title="Status of Genetic test"
+          onPress={() => handleMenuPress("Status of Genetic test")}
+        />
+        <MenuItem
+          icon={<ShoppingCartIcon size={24} color={Colors.text} />}
+          title="Order Genetic test"
+          onPress={() => handleMenuPress("Order Genetic test")}
+        />
+        <MenuItem
+          icon={<ArrowRightOnRectangleIcon size={24} color={Colors.text} />}
+          title="Log Out"
+          onPress={() => handleMenuPress("Log Out")}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default ProfileTab;
-
-const styles = StyleSheet.create({});
---
+++ app/tabStyles/ProfileTabStyle.ts
@@ -0,0 +1,45 @@
+import { Colors, Fonts } from "@/constants/theme";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.bg,
+  },
+
+  content: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  profileSection: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingVertical: 30,
+  },
+  profileImageContainer: {
+    marginBottom: 15,
+  },
+  profileNameContainer: {
+    flex: 1,
+    marginLeft: 15,
+    textAlign: "center",
+  },
+  profileImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  profileImageText: {
+    fontSize: 24,
+    color: Colors.text,
+    fontFamily: Fonts.bold,
+    fontWeight: "bold",
+  },
+  profileName: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#000",
+  },
+});
+export default styles;
